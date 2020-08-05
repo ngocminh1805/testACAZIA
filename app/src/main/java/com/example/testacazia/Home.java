702fd06a183dc6e3dcade78e74981c7fd836fa07
ngocminh1805 <ngocminhmtp123@gmail.com>
2020-08-05 13:09:29 +0700

    update

--- app/src/main/java/com/example/testacazia/Home.java
+++ app/src/main/java/com/example/testacazia/Home.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
     TextView wel ;
     Bundle data;
+    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         wel = findViewById(R.id.welcomeText);
+        logout = findViewById(R.id.logoutBtn);
         Intent intent = getIntent();
         data = intent.getExtras();
 
@@ -24,6 +28,21 @@ public class Home extends AppCompatActivity {
 
         wel.setText("Welcome "+ name);
 
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(Home.this, Login.class);
+                startActivity(login);
 
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
